67f283822c7afdc35218941044549f2914f946a5
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-27 17:33:05 -0800

    done. finished in prework

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ document.querySelector(`main#main`).remove();
 newHeader.id = `victory`;
 newHeader.innerHTML = `Flatiron School is the champion!`;
 newHeader.className = `victory`;
+//This was finished in pre work
